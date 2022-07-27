965b97868cd62ec2c3981e4e32651be62edaebac
ditto-vlad <vlad@zngly.com>
2022-07-27 09:59:49 +0100

    taxonomy test default acf field

--- dev/acf/acf.php
+++ dev/acf/acf.php
@@ -1,3 +1,57 @@
 <?php
 
 // load acf configuration here
+
+// taxonomy
+
+if (function_exists('acf_add_local_field_group')) :
+
+    acf_add_local_field_group(array(
+        'key' => 'group_62dff4db33699',
+        'title' => 'Taxonomy',
+        'fields' => array(
+            array(
+                'key' => 'field_62dff4ea7e7f4',
+                'label' => 'taxonomy test',
+                'name' => 'taxonomy_test',
+                'type' => 'text',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'show_in_graphql' => 1,
+                'default_value' => '',
+                'placeholder' => '',
+                'prepend' => '',
+                'append' => '',
+                'maxlength' => '',
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'taxonomy',
+                    'operator' => '==',
+                    'value' => 'all',
+                ),
+            ),
+        ),
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+        'show_in_graphql' => 1,
+        'graphql_field_name' => 'fields',
+        'map_graphql_types_from_location_rules' => 0,
+        'graphql_types' => '',
+    ));
+
+endif;
