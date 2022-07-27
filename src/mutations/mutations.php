b49a630745d52fcc83b89075729156c022241bbe
ditto-vlad <vlad@zngly.com>
2022-07-27 09:58:04 +0100

    feat: working mutations

--- src/mutations/mutations.php
+++ src/mutations/mutations.php
@@ -56,7 +56,7 @@ class Mutations
                                         $field_name = $field['name'] . "_" . $sub_field['name'];
                                         $value = $input[$field['graphql_name']][$graphql_name];
 
-                                        self::updateField($post_id, $value, $field_name, $sub_field['type'], $type_name);
+                                        self::update_single_field($post_id, $value, $field_name, $sub_field['type'], $type_name);
                                     }
                                 }
                             }
@@ -111,7 +111,7 @@ class Mutations
 
                         // check if the field exists
                         if (isset($input[$field['graphql_name']]))
-                            self::updateField($post_id, $input[$field['graphql_name']], $field['name'], $field['type'], $type_name);
+                            self::update_single_field($post_id, $input[$field['graphql_name']], $field['name'], $field['type'], $type_name);
                     }
                 }
             }
@@ -119,7 +119,7 @@ class Mutations
 
 
 
-    public static function updateField($post_id, $value, $field_name, $field_type, $type_name)
+    public static function update_single_field($id, $value, $field_name, $field_type, $type_name)
     {
         // if file types are images or file, make sure an ID is passed
         // accept guid or ids
@@ -130,15 +130,23 @@ class Mutations
         if ($field_type === 'post_object')
             $value = ACFMUtils::mapPostIdsFromGids($value);
 
-        // @todo: use acf set/get/update/delete functions
-        // current implementation does not take into account the _field_key meta value
+        // @todo: add filter to custom post types
+        $custom_post_types = ["Category", "Tag", "Option", "Options"];
+        if (in_array($type_name, $custom_post_types)) {
+            $post_id = strtolower($type_name . '_' . $id); // https://www.advancedcustomfields.com/resources/update_field/
 
-        if (in_array($type_name, ["Category", "Tag"]))
-            update_term_meta($post_id, $field_name, $value);
-        else
-            /**
-             * update a standalone acf field
-             */
-            update_post_meta($post_id, $field_name, $value);
+            if (self::should_update($value)) update_field($field_name, $value, $post_id);
+            else delete_field($field_name, $post_id);
+        } else {
+            if (self::should_update($value)) update_field($field_name, $value, $id);
+            else delete_field($field_name, $id);
+        }
+    }
+
+    public static function should_update($value)
+    {
+        if ($value === "") return false;
+
+        return true;
     }
 }
